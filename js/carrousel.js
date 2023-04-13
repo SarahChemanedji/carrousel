609d8d6ee1a7aabfd70626b69c12b445587e2f25
SarahChemanedji <e2075390@cmaisonneuve.qc.ca>
2023-04-13 11:27:35 -0400

    S11c1 insertion des img dans la boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,8 +1,38 @@
 (function(){
-    console.log('Debut du carrousel');
-    let btnCarrousel__ouvrir = document.querySelector('.carrousel_ouvrir');
+    console.log('début carrousel')
+    //Pour le bouton
+    let boutcarrousel__ouvrir = document.querySelector('.carrousel__ouvrir');
+
+    //Variable
     let carrousel = document.querySelector('.carrousel');
-    btnCarrousel__ouvrir.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--ouvrir');
+    let carrousel__x = document.querySelector('.carrousel__x');
+    let galerie = document.querySelector(".galerie");
+    let galerie__img = galerie.querySelectorAll("img");
+    // console.log (galerie__img.length)
+
+
+    boutcarrousel__ouvrir.addEventListener('mousedown',function(){
+        carrousel.classList.add('carrousel--ouvrir')
+        ajouter_img_dans_carrousel();
     })
+    carrousel__x.addEventListener('mousedown',function(){
+        carrousel.classList.remove('carrousel--ouvrir')
+
+    })
+/**
+ * 
+ * Ajouter_img_dans_carrousele
+ * Ajouter l'ensemble des images de la galerie dans la boite mondale carrouse;
+ * 
+ */
+function ajouter_img_dans_carrousel(){
+    for (const elm of galerie__img){
+        //console.log(elm.getAttribute('src'))
+        let img =document.createElement('img');
+        img.setAttribute('src',elm.getAttribute('src') )
+        console.log(img.getAttribute('src'))
+    }
+}
+
+
 })()
