550efd30df963fdb56fce991a1f58ba222922e57
SarahChemanedji <e2075390@cmaisonneuve.qc.ca>
2023-05-02 09:40:43 -0400

    s13c2 retirer bouton pour ouvrir carrousel

--- carrousel.php
+++ carrousel.php
@@ -40,8 +40,7 @@ add_action('wp_enqueue_scripts', 'enfiler_script_css' );
 
 function genere_boite()
 {
-    return '<button class="carrousel__ouvrir">Ouvrir Carrousel</button>
-    
+    return ' 
             <div class="carrousel">
             <i class="flechDroite"></i>
             <i class="flechGauche"></i>
